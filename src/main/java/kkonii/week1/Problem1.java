987c2a76c164c8d0b211c465aeafb76655b2a553
kkonii <119216472+kkonii@users.noreply.github.com>
2023-08-28 00:03:50 +0900

    feat : 입력값 검증 기능 작성

--- src/main/java/kkonii/week1/Problem1.java
+++ src/main/java/kkonii/week1/Problem1.java
@@ -1,8 +1,34 @@
 package kkonii.week1;
 
+import java.util.List;
 public class Problem1 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+    public static String solution(List<Integer> player) {
+
+    }
+    static class Range{
+        public boolean range(List<Integer> player){
+            return isContinuous(player) && isCorrectRange(player) && isCorrectLength(player);
+        }
+
+        private boolean isCorrectRange(List<Integer> player){
+            if(player.get(0) > 1 && player.get(0) < 400 && player.get(1) > 1 && player.get(1) < 400) {
+                return true;
+            }
+            return false;
+        }
+
+        private boolean isCorrectLength(List<Integer> player){
+            if(player.size() != 2){
+                return false;
+            }
+            return true;
+        }
+
+        private boolean isContinuous(List<Integer> player){
+            if(player.get(1) - player.get(0) != 1){
+                return false;
+            }
+            return true;
+        }
     }
 }
