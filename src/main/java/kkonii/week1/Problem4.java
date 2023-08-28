9ad5ac69deb5009a4ace9f0ee58d8e1ddb4c203f
kkonii <119216472+kkonii@users.noreply.github.com>
2023-08-29 05:12:31 +0900

    feat : 알파벳 변환 기능 완료

--- src/main/java/kkonii/week1/Problem4.java
+++ src/main/java/kkonii/week1/Problem4.java
@@ -2,7 +2,13 @@ package kkonii.week1;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] spellings  = word.toCharArray();
+
+        for(int i=0;i<spellings.length;i++){
+            if(Character.isLowerCase(spellings[i])) spellings[i] = (char)(219 - spellings[i]);
+            if(Character.isUpperCase(spellings[i])) spellings[i] = (char)(155 - spellings[i]);
+        }
+
+        return String.valueOf(spellings);
     }
 }
