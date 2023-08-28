cacc67611e7f95bdbd7336ffde41da200334f222
kkonii <119216472+kkonii@users.noreply.github.com>
2023-08-29 02:47:25 +0900

    feat : 중복되는 문자 삭제 기능 완료 (Probelm 2)

--- src/main/java/kkonii/week1/Problem2.java
+++ src/main/java/kkonii/week1/Problem2.java
@@ -1,8 +1,27 @@
 package kkonii.week1;
 
 public class Problem2 {
+    static Process process = new Process();
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuffer cryptoBuffer = new StringBuffer(cryptogram);
+
+        return process.removeDuplicateIndexes(cryptoBuffer);
+    }
+
+    static class Process{
+        private Integer firstDuplicatedIndex(StringBuffer buffer){
+            for(int i=0;i<buffer.length()-1;i++){
+                if(buffer.charAt(i) == buffer.charAt(i+1)) return i;
+            }
+            return -1;
+        }
+
+        private String removeDuplicateIndexes(StringBuffer buffer){
+            while(firstDuplicatedIndex(buffer) != -1){
+                buffer.delete(firstDuplicatedIndex(buffer),firstDuplicatedIndex(buffer)+2);
+            }
+            return buffer.toString();
+        }
     }
 }
