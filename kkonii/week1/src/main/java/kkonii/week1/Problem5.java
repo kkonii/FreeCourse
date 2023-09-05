51e3f18e190d995f3060cc0ec961f315e5233cf1
kkonii <119216472+kkonii@users.noreply.github.com>
2023-09-05 13:41:03 +0900

    Feat : 반환되는 금액을 배열에 저장하는 기능 구현 (problem 5)

--- kkonii/week1/src/main/java/kkonii/week1/Problem5.java
+++ kkonii/week1/src/main/java/kkonii/week1/Problem5.java
@@ -1,11 +1,19 @@
 package kkonii.week1;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        Integer[] coin = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int i=0;i<coin.length;i++){
+            answer.add(i, money / coin[i]);
+            money %= coin[i];
+        }
+
         return answer;
     }
 }
