1d1c3b370ad1bdeac8c1fd239a51c146ddee197d
kkonii <119216472+kkonii@users.noreply.github.com>
2023-09-16 03:06:20 +0900

    Feat : 입력값이 숫자가 아닐 경우 에러메세지 출력

--- kkonii/week3/java-lotto-main/src/main/java/valid/ValidMessage.java
+++ kkonii/week3/java-lotto-main/src/main/java/valid/ValidMessage.java
@@ -12,4 +12,12 @@ public class ValidMessage {
             throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위로 입력해야 합니다.");
         }
     }
+
+    private static void validateType(String money) {
+        String regex = "^[0-9]*$";
+
+        if (!money.matches(regex)) {
+            throw new IllegalArgumentException("[ERROR] 금액은 숫자 이외의 문자는 입력할 수 없습니다.");
+        }
+    }
 }
