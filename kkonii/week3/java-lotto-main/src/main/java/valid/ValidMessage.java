c3a682d6b17c5b4380a0c92a902e225a105c40e3
kkonii <119216472+kkonii@users.noreply.github.com>
2023-09-16 03:05:50 +0900

    Feat : 입력값이 1000원 단위가 아닐 경우 에러메세지 출력

--- kkonii/week3/java-lotto-main/src/main/java/valid/ValidMessage.java
+++ kkonii/week3/java-lotto-main/src/main/java/valid/ValidMessage.java
@@ -6,4 +6,10 @@ public class ValidMessage {
             throw new IllegalArgumentException("[ERROR] 최소 입력 금액은 1000원 입니다.");
         }
     }
+
+    private static void validateUnit(Integer price) {
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위로 입력해야 합니다.");
+        }
+    }
 }
