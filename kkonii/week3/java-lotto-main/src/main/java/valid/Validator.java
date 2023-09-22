26f3c8dca714583dc641cb32e9f1438ea9d53364
kkonii <119216472+kkonii@users.noreply.github.com>
2023-09-23 01:32:49 +0900

    Refactor : ValidMessageView를 두 개의 클래스로 분리
    - 에러메시지를 담고 있는 클래스, 유효성을 검증해서 메세지를 출력하는 클래스로 분리했습니다.

---
+++ kkonii/week3/java-lotto-main/src/main/java/valid/Validator.java
@@ -0,0 +1,33 @@
+package valid;
+
+import domain.ExceptionMessage;
+
+public class Validator {
+    private String regex = "^[0-9]*$";
+
+    public void validatePrice(String money) {
+        if (!money.matches(regex)) {
+            throw new IllegalArgumentException(ExceptionMessage.LOTTO_TYPE_ERROR.getMessage());
+        }
+
+        Integer price = Integer.valueOf(money);
+
+        if (price < 1000) {
+            throw new IllegalArgumentException(ExceptionMessage.MIN_PRICE_ERROR.getMessage());
+        }
+
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException(ExceptionMessage.PRICE_UNIT_ERROR.getMessage());
+        }
+    }
+
+    public void validateNumbers(String inputValue) {
+        String regex = "\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2}";
+
+        if (!inputValue.matches(regex)) {
+            throw new IllegalArgumentException(ExceptionMessage.REGEX_ERROR.getMessage());
+        }
+
+
+    }
+}
